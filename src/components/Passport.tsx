9ba4dff7be083ea053adb2586b4b2acfbf693d06
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2026-03-15 02:39:31 +0700

    fix(ui): set passport transform wrapper size

--- src/components/Passport.tsx
+++ src/components/Passport.tsx
@@ -49,7 +49,7 @@ export default function Passport() {
       }}
     >
       <TransformComponent
-        contentStyle={{ width: "100%", height: "100%" }}
+        wrapperStyle={{ width: "100%", height: "100%" }}
         wrapperProps={{ onMouseDown: onContainerMouseDown }}
       >
         <Card cardRef={cardRef} />
