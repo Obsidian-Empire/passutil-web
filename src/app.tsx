477f38c52ccc53f9e4e6f92d33c0e5d704feb164
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2026-03-15 02:42:14 +0700

    fix(ui): make app layout responsive on small screens

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Side } from "./components/Side";
 
 export function App() {
   return (
-    <div className="grid grid-cols-[70%_30%] h-svh">
+    <div className="grid grid-rows-[30%_70%] md:grid-rows-1 md:grid-cols-[70%_30%] md:h-svh">
       <Passport />
       <Side />
     </div>
